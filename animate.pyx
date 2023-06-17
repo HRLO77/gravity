8c733e6954a7e316ee640527265fc57c6c24b42c
HRLO77 <shakebmohammad.10@gmail.com>
2023-06-16 17:34:21 -0700

    fixed rendering

--- animate.pyx
+++ animate.pyx
@@ -15,37 +15,42 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from matplotlib.animation import PillowWriter
+from matplotlib.animation import FFMpegWriter
 import numpy as np
 cimport numpy as npc
-import time
-import datetime
+import pickle
 fig = plt.figure()
 
 #creating a subplot 
 ax1 = fig.add_subplot(1,1,1)
 
 import pickle
-cdef bint save = bool(int(input('Save animation as gif or render realtime? [1/0]: ')))
+cdef bint save = bool(int(input('Save animation as video or render realtime? [1/0]: ')))
 cdef float[:,:,:,] data
 with open('data.pickle', 'rb') as f:
-    data = np.array(pickle.load(f)[0],allow_pickle=True)
-cdef unsigned char[:,] con = np.array([2 for i in range(data.shape[1])], dtype=np.ubyte)
-cdef unsigned int c = 0
-cdef inline void animate(i):
-    
-    global c, con
-
-    ax1.clear()
-    ax1.scatter(*zip(*data[i]), con)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Gravitational simulation')
-    c += 1
+    data = np.array(pickle.load(f)[0], dtype=np.float32)
+cdef unsigned char[:,] con = np.array([1 for i in range(data.shape[1])], dtype=np.ubyte)
+cdef inline void animate(unsigned int i):
+    cdef float[:,:,] dat
+    try:
+        global c, con
+        ax1.clear()
+        dat = data[i]
+        ax1.scatter(*zip(*dat), con)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Gravitational simulation')
+    except BaseException:
+        if save:
+            ani.save(f'./{np.random.randint(0, 2_000_000)}.mp4', writer=FFMpegWriter(fps=20))
+        else:
+            plt.show()
 plt.ioff()
-ani = animation.FuncAnimation(fig, animate, interval=1 if save else 1000/30, cache_frame_data=data.shape[1] < 5_000, frames=data.shape[0], repeat=False)
-
-if save:
-    ani.save(f'./{np.random.randint(0, 2_000_000)}.mp4', writer=PillowWriter(fps=30))
-else:
-    plt.show()
+ani = animation.FuncAnimation(fig, animate, interval=1 if save else 1000/30, cache_frame_data=data.shape[1] < 3_000, frames=data.shape[0], repeat=False)
+try:
+    if save:
+        ani.save(f'./{np.random.randint(0, 2_000_000)}.mp4', writer=FFMpegWriter(fps=20))
+    else:
+        plt.show()
+except BaseException:
+    exit()
--- start_animation.py
+++ start_animation.py
@@ -4,7 +4,7 @@ else:
 
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
-    from matplotlib.animation import PillowWriter
+    from matplotlib.animation import FFMpegWriter
     import numpy as np
     fig = plt.figure()
 
@@ -16,24 +16,29 @@ else:
     with open('data.pickle', 'rb') as f:
         data = np.array(pickle.load(f)[0], dtype=np.float16)
 
-    con = np.array([2 for i in range(data.shape[1])], dtype=np.ubyte)
+    con = np.array([1 for i in range(data.shape[1])], dtype=np.ubyte)
     c = 0
     def animate(i):
         
         global c, con
     
-        
-        ax1.clear()
-        ax1.scatter(*zip(*data[i]), con)
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.title('Gravitational simulation')
-        c += 1
+        try:
+            ax1.clear()
+            ax1.scatter(*zip(*data[i]), con)
+            plt.xlabel('X')
+            plt.ylabel('Y')
+            plt.title('Gravitational simulation')
+            c += 1
+        except BaseException:
+            if save:
+                ani.save(f'./{np.random.randint(0, 2_000_000)}.gif', writer=FFMpegWriter(fps=20))
+            else:
+                plt.show()
 
     plt.ioff()
     ani = animation.FuncAnimation(fig, animate, interval=1 if save else 1000/30, cache_frame_data=data.shape[1] < 5_000, frames=data.shape[0])
 
     if save:
-        ani.save(f'./{np.random.randint(0, 2_000_000)}.gif', writer=PillowWriter(fps=30))
+        ani.save(f'./{np.random.randint(0, 2_000_000)}.gif', writer=FFMpegWriter(fps=20))
     else:
         plt.show()
