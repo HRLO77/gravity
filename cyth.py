5c0dfd9b699d989ee658eb57d8dbc2b96a1ed593
HRLO77 <shakebmohammad.10@gmail.com>
2023-07-24 12:32:55 -0700

    test

--- cyth.py
+++ cyth.py
@@ -9,7 +9,13 @@ import os
 include = [numpy.get_include(), 'gravity']
 pk=[*setuptools.find_packages('gravity'), 'gravity']
 cd = {'language_level' : "3"}
-args = ['/O2', '/fp:fast', '/Qfast_transcendentals']
+
+# args = ['/O2', '/fp:fast', '/Qfast_transcendentals'] # args for MSVC
+
+# args = ['-Ofast', '-ffast-math', '-fno-signed-zeros', '-faggressive-loop-optimizations', '-fcaller-saves', '-mtune=native', '-march=native', '-ffinite-loops', '-shared', '-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION'] # args for GCC
+
+# args = ['-Ofast', '-ffast-math', '-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION', '-mtune=native', '-march=native', '-fapprox-func', '-fno-honor-infinities', '-fno-honor-nans', '-cl-mad-enable', '-cl-no-signed-zeros', '-ffinite-loops', '-cl-fast-relaxed-math', '-shared'] # args for clang
+
 setup(
     ext_modules=cythonize([Extension("gravity.constants_cy", sources=["gravity/constants_cy.pyx"], include_dirs=include, extra_compile_args=args)], nthreads=12, compiler_directives=cd, show_all_warnings=True),
     zip_safe=False, include_dirs=include, packages=pk
